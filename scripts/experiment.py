c141539e40888d5f1cbe84afa261e732de9eec9d
lxhq <lxheqiao@163.com>
2023-11-17 16:31:22 +0800

    Change the location of the training dataset

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -15,7 +15,7 @@ def generate_args(binary, *params):
     arguments.extend(list(params))
     return arguments
 
-def run_train(dataset, epochs, saved_results_name, output_file):
+def run_train(dataset, epochs, saved_results_name, output_file, full_data_dir):
     command = generate_args("python3 src/main.py",
                             "--num_epoch", str(epochs), 
                             "--sample_method", "induced", 
@@ -23,10 +23,10 @@ def run_train(dataset, epochs, saved_results_name, output_file):
                             "--model_name", "wasserstein", 
                             "--device", "cuda",
                             "--graph_file", dataset,
-                            "--data_graph_path", "data/{}/{}.graph".format(dataset, dataset), 
-                            "--true_card_path", "data/{}/query_graph_labels.csv".format(dataset), 
-                            "--train_folder", "data/{}/train/".format(dataset),
-                            "--test_folder", "data/{}/test/".format(dataset),
+                            "--data_graph_path", "{}/{}.graph".format(full_data_dir, dataset), 
+                            "--true_card_path", "{}/query_graph_labels.csv".format(full_data_dir), 
+                            "--train_folder", "{}/train/".format(full_data_dir),
+                            "--test_folder", "{}/test/".format(full_data_dir),
                             "--query_vertex_num", "all",
                             "--saved_name", saved_results_name,
                             "--stdout_file", output_file)
@@ -34,17 +34,11 @@ def run_train(dataset, epochs, saved_results_name, output_file):
 
 if __name__ == '__main__':
     dataset = 'yeast'
-    epochs = 30
-
-    dataset_dir = 'data/{}/'.format(dataset)
-    train_dir = dataset_dir + 'train/'
-    test_dir = dataset_dir + 'test/'
-
+    epochs = 100
+    full_data_dir = '/home/lxhq/Documents/workspace_1/dataset/ml_data/{}'.format(dataset)
     output_dir = 'outputs/{}/'.format(dataset)
 
-    for idx in range(10):
+    for idx in range(1):
         model_name = 'baseline_{}'.format(idx)
         output_file = output_dir + '{}_{}'.format(epochs, model_name) + '.txt'
-        run_train(dataset, epochs, model_name, output_file)
-
-
+        run_train(dataset, epochs, model_name, output_file, full_data_dir)
