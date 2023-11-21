a5c7068bba47cd6cecdec5a348caa8f38df105e7
lxhq <lxheqiao@163.com>
2023-11-21 13:36:25 +0800

    fix some bugs in the experiment script

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -2,21 +2,22 @@ from subprocess import Popen, PIPE
 
 def execute_binary(args):
     process = Popen(' '.join(args), shell=True, stdout=PIPE, stderr=PIPE)
-    (std_output, std_error) = process.communicate()
-    process.wait()
-    rc = process.returncode
-    if std_error:
-        print(std_error.decode(), flush=True)
-        exit(-1)
-    return rc, std_output, std_error
+    # (std_output, std_error) = process.communicate()
+    return process
+    # process.wait()
+    # rc = process.returncode
+    # if std_error:
+    #     print(std_error.decode(), flush=True)
+    #     exit(-1)
+    # return rc, std_output, std_error
 
 def generate_args(binary, *params):
     arguments = [binary]
     arguments.extend(list(params))
     return arguments
 
-def run_train(dataset, epochs, saved_results_name, full_data_dir):
-    command = generate_args("python3 src/main.py",
+def get_command(dataset, epochs, saved_results_name, full_data_dir):
+    return generate_args("python3 src/main.py",
                             "--num_epoch", str(epochs), 
                             "--model_name", "wasserstein", 
                             "--graph_file", dataset,
@@ -25,13 +26,16 @@ def run_train(dataset, epochs, saved_results_name, full_data_dir):
                             "--train_folder", "{}/train/".format(full_data_dir),
                             "--test_folder", "{}/test/".format(full_data_dir),
                             "--saved_name", saved_results_name)
-    execute_binary(command)
 
 if __name__ == '__main__':
     dataset = 'yeast'
     epochs = [30, 50, 70, 90, 100, 120, 140, 150]
     full_data_dir = '/home/lxhq/Documents/workspace_1/dataset/ml_data/{}'.format(dataset)
-
+    processes = []
     for epoch in epochs:
         saved_result_name = 'baseline'
-        run_train(dataset, epochs, saved_result_name, full_data_dir)
+        command = get_command(dataset, epoch, saved_result_name, full_data_dir)
+        processes.append(execute_binary(command))
+    
+    for process in processes:
+        process.wait()
