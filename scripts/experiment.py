47343c9b7b999b38aa1aa7f88eb0b5e792d2eacd
lxhq <lxheqiao@163.com>
2023-11-22 12:16:55 +0800

    add code for training models with different epochs and lrs

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -16,7 +16,7 @@ def generate_args(binary, *params):
     arguments.extend(list(params))
     return arguments
 
-def get_command(dataset, epochs, saved_results_name, output_file, full_data_dir, lr):
+def get_command(dataset, epochs, saved_result_name, output_file, full_data_dir, lr):
     return generate_args("python3 src/main.py",
                             "--learning_rate", str(lr),
                             "--num_epoch", str(epochs), 
@@ -30,22 +30,23 @@ def get_command(dataset, epochs, saved_results_name, output_file, full_data_dir,
                             "--train_folder", "{}/train/".format(full_data_dir),
                             "--test_folder", "{}/test/".format(full_data_dir),
                             "--query_vertex_num", "all",
-                            "--saved_name", saved_results_name,
+                            "--saved_name", saved_result_name,
                             "--stdout_file", output_file)
 
 if __name__ == '__main__':
     dataset = 'yeast'
-    epochs = 150
     full_data_dir = '/home/lxhq/Documents/workspace_1/dataset/ml_data/{}'.format(dataset)
     output_dir = 'outputs/{}/'.format(dataset)
-    lrs = [5e-3, 1e-3, 5e-4, 1e-4]
+    epochs = [30, 60, 90, 120, 150]
+    lrs = [5e-3, 3e-3, 1e-3, 8e-4, 5e-4, 3e-4, 1e-4]
 
     processes = []
-    for lr in lrs:
-        model_name = 'better_performance_mse_log2_dropout_scheduler_lr_{}'.format(lr)
-        output_file = output_dir + '{}_{}'.format(epochs, model_name) + '.txt'
-        command = get_command(dataset, epochs, model_name, output_file, full_data_dir, lr)
-        processes.append(execute_binary(command))
-    
-    for process in processes:
-        process.wait()
+    for epoch in epochs:
+        for lr in lrs:
+            saved_result_name = '{}_lr_{}_better_performance_mse_log2_dropout_scheduler'.format(epoch, lr)
+            output_file = output_dir + saved_result_name + '.txt'
+            command = get_command(dataset, epoch, saved_result_name, output_file, full_data_dir, lr)
+            processes.append(execute_binary(command))
+        
+        for process in processes:
+            process.wait()
